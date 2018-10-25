825888a06c730523d58b778f36e3750fc3836afd
YeClimEric <yeclimeric@gmail.com>
2018-10-25 17:14:30 +0800

    add gen card tools code

---
+++ genVcard/run.js
@@ -0,0 +1,25 @@
+const fs = require( 'fs-extra' );
+const quotePrintable = require( 'quoted-printable' );
+const utf8 = require( 'utf8' );
+
+
+function appendFile( file, txt ) {
+    fs.outputFile( file, txt, {
+        flag: "a+"
+    }, ( err ) => {} );
+}
+
+let cardModule = "BEGIN:VCARD\nVERSION:2.1\nN;CHARSET=UTF-8;ENCODING=QUOTED-PRINTABLE:;name;;;\nFN;CHARSET=UTF-8;ENCODING=QUOTED-PRINTABLE:fname\nTEL;CELL:mobile\nEND:VCARD"
+
+
+let data = fs.readFileSync( "./m" ).toString();
+
+data.split( "\n" ).forEach( ( line ) => {
+    d = line.split( ',' )
+    if ( d.length != 2 ) {
+        return;
+    }
+    name = quotePrintable.encode( utf8.encode( d[ 1 ] ) );
+    mobile = d[ 0 ]
+    appendFile( "./cus_card_vcf", cardModule.replace( "name", name ).replace( "fname", name ).replace( 'mobile', mobile ) );
+} )
