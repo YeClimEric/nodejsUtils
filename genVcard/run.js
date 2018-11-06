a3f00051e3805094d2f5b56fc80f88a8bf29889e
YeClimEric <yeclimeric@gmail.com>
2018-11-06 20:31:08 +0800

    modify file

--- genVcard/run.js
+++ genVcard/run.js
@@ -1,25 +1,31 @@
-const fs = require( 'fs-extra' );
-const quotePrintable = require( 'quoted-printable' );
-const utf8 = require( 'utf8' );
+"use strict";
 
-function appendFile( file, txt ) {
-    fs.outputFile( file, txt, {
+const quotePrintable = require("quoted-printable");
+const utf8 = require("utf8");
+
+import {
+    readFileSync,
+    outputFile
+} from "fs";
+
+
+function appendFile(file, txt) {
+    outputFile(file, txt, {
         flag: "a+"
-    }, ( err ) => {} );
+    }, () => {});
 }
 
 let cardModule = "BEGIN:VCARD\nVERSION:2.1\nN;CHARSET=UTF-8;ENCODING=QUOTED-PRINTABLE:;name;;;\nFN;CHARSET=UTF-8;ENCODING=QUOTED-PRINTABLE:fname\nTEL;CELL:mobile\nEND:VCARD\n"
 
+let data = readFileSync("./data.txt").toString();
 
-let data = fs.readFileSync( "./data.txt" ).toString();
-
-data.split( "\n" ).forEach( ( line ) => {
-    d = line.split( ',' )
-    if ( d.length != 2 ) {
+data.split("\n").forEach((line) => {
+    let d = line.split(",");
+    if (d.length !== 2) {
         return;
     }
-    let name = quotePrintable.encode( utf8.encode( d[ 1 ] ) );
-    let mobile = d[ 0 ];
-    mobile = mobile.slice( 0, 3 ) + " " + mobile.slice( 3, 6 ) + " " + mobile.slice( 6 );
-    appendFile( "./cus_card.vcf", cardModule.replace( "name", name ).replace( "fname", name ).replace( 'mobile', mobile ) );
-} )
+    let name = quotePrintable.encode(utf8.encode(d[1]));
+    let mobile = d[0];
+    mobile = mobile.slice(0, 3) + " " + mobile.slice(3, 6) + " " + mobile.slice(6);
+    appendFile("./cus_card.vcf", cardModule.replace("name", name).replace("fname", name).replace('mobile', mobile));
+});
